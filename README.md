d5468661b61d26b34626cbbeb629898aed20aed8
Cody Barr <cody.barr@gmail.com>
2019-10-31 20:34:35 -0400

    Added more shortcuts
    You can now move the current tab directly to position 1-8, or last. Also updated readme with instructions for setting initial shortcuts for 2-8.

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # TabRearranger
 
-Simple chrome extension for quickly rearranging tabs using keyboard shortcuts
+[View on the Chrome Web Store](https://chrome.google.com/webstore/detail/tab-rearranger/nkbnnecfgmajcnadoaldipigoaamkddo)
+
+Simple chrome extension for quickly rearranging tabs using keyboard shortcuts.
+
+## Shortcuts
+
+Google is cool and only lets you pre-define 4 keyboard shortcuts for an extension, so to update them you'll have to visit shortcut settings for your browser:
+
+-   [brave://extensions/shortcuts](brave://extensions/shortcuts)
+-   [chrome://extensions/shortcuts](chrome://extensions/shortcuts)
+
+There are basically 4 different types of shortcuts though (using OSX as an example)
+
+### ⌥⇧→
+
+`Alt+Shift+Right`
+
+Advance current tab one position to the right (forward). Will wrap.
+
+### ⌥⇧←
+
+`Alt+Shift+Left`
+
+Advance current tab one position to the left (backwards). Will wrap.
+
+### ⌥⇧{1-8}
+
+`Alt+Shift+{1-8}`
+
+Move current tab to position of provided number.
+
+_Note: The only keyboard shorcut set up for this command is `1`. You'll have to visit the links above to set the other keyboard shortcuts for 2-8._
+
+### ⌥⇧9
+
+`Alt+Shift+9`
+
+Move current tab to end of tabs in current window.
--- background.js
+++ background.js
@@ -1,31 +1,38 @@
-
 chrome.commands.onCommand.addListener(function(command) {
+	chrome.tabs.query({ currentWindow: true }, function(allTabs) {
+		var lastIndex = allTabs[allTabs.length - 1].index // gets the index of the last tab
 
-  chrome.tabs.query({currentWindow:true}, function(allTabs) {
-    var lastIndex = allTabs[allTabs.length - 1].index // gets the index of the last tab
+		chrome.tabs.query({ active: true, currentWindow: true }, function(tab) {
+			var tab = tab[0] // only one active tab per current window
+			var tab_location = tab.index
+			var tab_new_location = 0
 
-    chrome.tabs.query({active:true, currentWindow:true}, function(tab) {
+			if (command === 'move_tab_back') {
+				tab_new_location = tab_location - 1
+			} else if (command === 'move_tab_forward') {
+				tab_new_location =
+					tab_location === lastIndex ? 0 : tab_location + 1
+			} else if (command === 'move_to_position_1') {
+				tab_new_location = 0
+			} else if (command === 'move_to_position_2') {
+				tab_new_location = 1
+			} else if (command === 'move_to_position_3') {
+				tab_new_location = 2
+			} else if (command === 'move_to_position_4') {
+				tab_new_location = 3
+			} else if (command === 'move_to_position_5') {
+				tab_new_location = 4
+			} else if (command === 'move_to_position_6') {
+				tab_new_location = 5
+			} else if (command === 'move_to_position_7') {
+				tab_new_location = 6
+			} else if (command === 'move_to_position_8') {
+				tab_new_location = 7
+			} else if (command === 'move_to_position_9') {
+				tab_new_location = lastIndex
+			}
 
-      var tab = tab[0] // only one active tab per current window
-      var tab_location = tab.index
-      var tab_new_location = 0
-      if (command == 'move_tab_back')
-        tab_new_location = tab_location - 1
-      else if (command == 'move_tab_forward') {
-        if (tab_location == lastIndex)
-          tab_new_location = 0
-        else
-          tab_new_location = tab_location + 1
-      }
-
-      console.log('Last index:', lastIndex)
-      console.log('Tab', tab.id, 'was moved from position', tab_location, 'to', tab_new_location);
-
-      chrome.tabs.move(tab.id, {index: tab_new_location})
-    });
-
-  });
-
-
-
-});
+			chrome.tabs.move(tab.id, { index: tab_new_location })
+		})
+	})
+})
